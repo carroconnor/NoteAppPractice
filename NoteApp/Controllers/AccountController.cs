aff93f85595063e19f0343c737929728d15a251e
Carr OConnor <carr.oconnor@gmail.com>
2019-01-25 15:35:36 -0500

    DB SetUp

--- NoteApp/Controllers/AccountController.cs
+++ NoteApp/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using NoteApp.Data;
 using NoteApp.Models;
 
 namespace NoteApp.Controllers
