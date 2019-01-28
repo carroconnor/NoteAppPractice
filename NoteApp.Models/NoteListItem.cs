0e9e61c4592c870d37cac6a84ecf888c05e89476
Carr OConnor <carr.oconnor@gmail.com>
2019-01-28 15:39:02 -0500

    Note Controller Added

---
+++ NoteApp.Models/NoteListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NoteApp.Models
+{
+    public class NoteListItem
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ NoteApp/Controllers/NoteController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using NoteApp.Models;
+
+namespace NoteApp.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
